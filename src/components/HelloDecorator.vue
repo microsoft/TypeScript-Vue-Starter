9c9caf444c04cc777498ddb5ec59ffab39090197
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2017-11-06 13:00:06 -0800

    Reorder imports for consistency.

--- src/components/HelloDecorator.vue
+++ src/components/HelloDecorator.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Vue, Component, Prop } from "vue-property-decorator";
 
 @Component
 export default class HelloDecorator extends Vue {
